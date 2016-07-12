eddbe59f430c7ee1fbd3c78d30c7f2a731575957
menno <mennopet@gmail.com>
2016-07-12 16:34:41 +0200

    exclusfive -> button in in voorraad is weer gecenter aligned

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/Users/fabian/.rvm/gems/ruby-2.2.2/bin/wkhtmltopdf'
+  :exe_path => '/Users/menno/.rvm/rubies/ruby-2.2.2/bin/wkhtmltopdf'
 }
