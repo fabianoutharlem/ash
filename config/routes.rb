4d887f7663a3061f6b1bfeec2062eaac48b4946b
Fabian Oudhaarlem <fabian@coqtail.com>
2016-07-14 18:04:34 +0200

    fix phrasing route

--- config/routes.rb
+++ config/routes.rb
@@ -109,6 +109,18 @@ Rails.application.routes.draw do
     end
   end
 
+  resources Phrasing.route, as: 'phrasing_phrases', controller: 'phrasing_phrases', only: [:index, :edit, :update, :destroy] do
+    collection do
+      get 'help'
+      get 'import_export'
+      get 'sync'
+      get 'download'
+      post 'upload'
+      put 'remote_update_phrase'
+    end
+  end
+  resources :phrasing_phrase_versions, as: 'phrasing_phrase_versions', controller: 'phrasing_phrase_versions', only: [:destroy]
+
   resources :pages, only: [:show], path: '/'
 
 end
