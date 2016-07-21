3c20f420792d8957827e120df71ecf149d6d9a6a
menno <mennopet@gmail.com>
2016-07-21 19:08:58 +0200

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require jquerytouch.js
 //= require jquery.marquee.min.js
 //= require owl.carousel.min.js
 //= require top_car_carousel.js
