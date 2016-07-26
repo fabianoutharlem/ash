2780aa250f06500cf0292c5556fde9969cbcb428
Fabian Oudhaarlem <fabian@coqtail.com>
2016-07-26 13:28:55 +0200

    homepage actie slide now with link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726105948) do
+ActiveRecord::Schema.define(version: 20160726112558) do
 
   create_table "alternatives", force: :cascade do |t|
     t.string   "which",        limit: 255
