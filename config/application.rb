38fd7b9588076256add32c3d7253335120606272
menno <mennopet@gmail.com>
2016-07-27 18:53:13 +0200

    lightslider fix

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Ash
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += [ 'car_card.css', 'vendor/*.css', 'admin/appviews.css', 'admin/cssanimations.css', 'admin/dashboards.css', 'admin/forms.css', 'admin/gallery.css', 'admin/graphs.css', 'admin/mailbox.css', 'admin/miscellaneous.css', 'admin/pages.css', 'admin/tables.css', 'admin/uielements.css', 'admin/widgets.css', 'admin/commerce.css' ]
+    config.assets.precompile += [ 'car_card.css', 'vendor/lightslider.css', 'admin/appviews.css', 'admin/cssanimations.css', 'admin/dashboards.css', 'admin/forms.css', 'admin/gallery.css', 'admin/graphs.css', 'admin/mailbox.css', 'admin/miscellaneous.css', 'admin/pages.css', 'admin/tables.css', 'admin/uielements.css', 'admin/widgets.css', 'admin/commerce.css' ]
     config.assets.precompile += [ 'admin/appviews.js', 'admin/cssanimations.js', 'admin/dashboards.js', 'admin/forms.js', 'admin/gallery.js', 'admin/graphs.js', 'admin/mailbox.js', 'admin/miscellaneous.js', 'admin/pages.js', 'admin/tables.js', 'admin/uielements.js', 'admin/widgets.js', 'admin/commerce.js' ]
     config.assets.precompile += %w( .svg .eot .woff .ttf )
 
