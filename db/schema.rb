85bb0e34e5839f06351ae88c44addc9536fbae93
Fabian Oudhaarlem <fabian@coqtail.com>
2016-08-31 12:53:18 +0200

    add more colorpickers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831104432) do
+ActiveRecord::Schema.define(version: 20160831105240) do
 
   create_table "alternatives", force: :cascade do |t|
     t.string   "which",        limit: 255
