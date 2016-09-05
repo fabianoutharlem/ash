b682c9d69d791ab5a22c4dd97183fbff2a951158
Fabian Oudhaarlem <fabian@coqtail.com>
2016-09-05 19:19:59 +0200

    add error code breadcrumbs

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,8 +2,8 @@ class ErrorsController < ApplicationController
 
   def show
     status_code = params[:code] || 500
+    add_breadcrumb status_code.to_s
     render status_code.to_s, status: status_code
-    add_breadcrumb status_code
   end
 
 end
