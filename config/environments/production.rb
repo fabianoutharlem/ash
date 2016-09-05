dfe484f0239f69c309b7d054027cb735f856879d
Fabian Oudhaarlem <fabian@coqtail.com>
2016-09-05 18:42:19 +0200

    asset host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.routes.default_url_options[:host] = config.domain
-
+  config.asset_host = config.domain
+  config.action_controller.asset_host = config.domain
   config.action_mailer.asset_host = config.domain
 end
