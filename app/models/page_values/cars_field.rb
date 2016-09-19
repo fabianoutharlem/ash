47167a6e2cd8f38690e5c77fff08d4911f7b2053
Fabian Oudhaarlem <fabian@coqtail.com>
2016-09-19 14:31:55 +0200

    fix for disappearing car

--- app/models/page_values/cars_field.rb
+++ app/models/page_values/cars_field.rb
@@ -5,7 +5,7 @@ module PageValues
     serialize :value
 
     def cars
-      Car.find(value.reject(&:blank?)) if value.present? && value.any? rescue Car.none
+      Car.where(id: value.reject(&:blank?)) if value.present? && value.any? rescue Car.none
     end
 
     def form_field
