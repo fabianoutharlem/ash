191d0f672fa923715ed24cb83130f29cf6852806
Fabian Oudhaarlem <fabian@coqtail.com>
2016-09-23 18:17:38 +0200

    caching

--- app/models/car.rb
+++ app/models/car.rb
@@ -38,7 +38,7 @@ class Car < ActiveRecord::Base
   belongs_to :body_type
   belongs_to :fuel_type
   belongs_to :model
-  belongs_to :brand
+  belongs_to :brand, touch: true
   belongs_to :transmission_type
   has_many :car_medias, dependent: :destroy
 
