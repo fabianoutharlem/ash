42193551cf2a60bf374b0fdad285894e6ea49612
Fabian Oudhaarlem <fabian@coqtail.com>
2016-09-26 17:30:50 +0200

    conditional asset host for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = 'ash-' + Rails.env
   config.aws_acl    = 'public-read'
-  config.asset_host = 'd1ocmpidlz0zue.cloudfront.net'
+  config.asset_host = 'd1ocmpidlz0zue.cloudfront.net' if Rails.env.production?
 
   # The maximum period for authenticated_urls is only 7 days.
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
